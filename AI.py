d7ee088008fc35cb59513351e1b4fc37a5c5969f
Ayun-xin <2582357590@qq.com>
2025-05-28 20:30:33 +0800

    Update AI.py

--- AI.py
+++ AI.py
@@ -1,4 +1,12 @@
 # -*- coding: utf-8 -*-
+import os
+os.system(r"python D:\个人情报站\GLM-4工具.py")
+all_news=['爬虫工具','网页前端后端工具','视频剪辑工具','音乐创作工具']
+all_continue=''
+for t in all_news:
+    with open('{}.txt'.format(t), 'r', encoding='utf-8') as file:
+        content = file.read()
+    all_continue+=content
 
 
 import os
@@ -13,7 +21,7 @@ client = OpenAI(
 completion = client.chat.completions.create(
     model="deepseek-r1",  # 此处以 deepseek-r1 为例，可按需更换模型名称。
     messages=[
-        {'role': 'user', 'content': "\n流媒体能力\n1.\t文案剧情编写能力\n2.\t视频剪辑能力\n3.\t图片ps能力\n计算机编程能力\n4.\t网站全栈制作能力\n5.\t网页爬虫制作能力\n6.\t爬虫自动化\n7.\t爬虫逆向\n8.\t分布式爬虫数据清洗\n9.\t微信小程序制作\n10.\t算法编程能力\n11.\t初级小游戏制作能力\n音乐能力\n12.\t歌词编写\n13.\t唱歌\n撰写小说能力\n14.\t强灵感能力\n15.\t大批量剧情撰写能力\n辅助游戏+服务器能力\n16.\t游戏服务器搭建\n17.\t个人网盘搭建\n18.\t服务器维护\n这是我的所有能力,请为我依据我的能力为我推荐当下辅助我能力最好用的工具和新型信息,同时由于我处于被心上人长期冷落的境地为我贴心送上一句安慰的话语"}
+        {'role': 'user', 'content':"这些是最新的关于我喜欢的工具的消息"+all_continue+ "请你根据它再依据我的\n流媒体能力\n1.\t文案剧情编写能力\n2.\t视频剪辑能力\n3.\t图片ps能力\n计算机编程能力\n4.\t网站全栈制作能力\n5.\t网页爬虫制作能力\n6.\t爬虫自动化\n7.\t爬虫逆向\n8.\t分布式爬虫数据清洗\n9.\t微信小程序制作\n10.\t算法编程能力\n11.\t初级小游戏制作能力\n音乐能力\n12.\t歌词编写\n13.\t唱歌\n撰写小说能力\n14.\t强灵感能力\n15.\t大批量剧情撰写能力\n辅助游戏+服务器能力\n16.\t游戏服务器搭建\n17.\t个人网盘搭建\n18.\t服务器维护\n这些能力为我分析出对我有用的工具,然后再生成一句鼓励我的话"}
     ]
 )
 
